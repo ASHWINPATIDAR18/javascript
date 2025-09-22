9e0321b6a6cf9bac57d769d1fafae63fd4a52a67
ASHWINPATIDAR18 <ashwinpatidar2000@gmail.com>
2025-09-22 12:59:18 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,75 @@
+fullName="Tony Stark";
+console.log("fullName");
+console.log(fullName);
+
+age=24;
+price=99.99;
+q=null;
+r=undefined;
+console.log(age);
+console.log(q);
+console.log(r);
+
+_fullName="Tony Stark";
+$fullName="Tony Stark";
+console.log(_fullName);
+console.log($fullName);
+
+isFollow=false
+isFollow=true
+console.log(isFollow);
+
+apple="ashok";
+Apple="Ashok";
+console.log(apple);
+console.log(Apple);
+
+let name="Tony Stark";
+let Age=25;
+let totalPrice=1000;
+console.log(Age);
+
+let a;
+console.log(a);
+
+const b=10;
+console.log(b);
+
+let yourAge=24;
+let Price=100.50;
+yourName="tony stark";
+isFollowed=true;
+let x;
+let y=null;
+let z=BigInt("123");
+let p=Symbol("Hello!");
+console.log(yourAge);
+console.log(Price);
+console.log(yourName);
+console.log(isFollowed);
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(p);
+
+const student={
+    fullName:"Rahul Kumar",
+    age:20,
+    cgpa:8.2,
+    isPass:true,
+};
+
+console.log(student.age);
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+student["fullName"]="Rahul Sharma";
+console.log[student.fullName];
+student["isPass"]=false;
+console.log[student.isPass];
+
+const profile={
+    userName:"@radheradhe",
+    isFollow:true,
+    followers:500000,
+    following:4,
+};
